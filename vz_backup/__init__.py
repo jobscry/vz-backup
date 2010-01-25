0027de795ef1a306722ff4eea2e58f526a7497d9
Joe Vasquez <joe.vasquez@gmail.com>
2010-01-24 22:59:45 -0500

    added version, author, email, license to init

---
+++ vz_backup/__init__.py
@@ -0,0 +1,6 @@
+# -*- mode: python; coding: utf-8; -*-
+VERSION = (0, 0, 1)
+__version__ = '.'.join(map(str, VERSION))
+__author__ = 'Joe Vasquez'
+__email__ = 'joe.vasquez@gmail.com'
+__license__ = 'MIT'
