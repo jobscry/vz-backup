83c0814f196d2cb2d9106219c5055fb2a27f812e
Joe Vasquez <joe.vasquez@gmail.com>
2010-04-29 17:49:17 -0400

    added pruning by size in bytes

--- vz_backup/models.py
+++ vz_backup/models.py
@@ -3,6 +3,7 @@
 from django.conf import settings
 from django.core.management.commands import dumpdata
 from django.db import models
+from django.db.models import Sum
 from django.db.models.signals import post_save, pre_delete
 
 import datetime
@@ -16,23 +17,27 @@ FORMAT = 'json'
 PRUNE_CHOICES = (
     ('count', 'Count'),
     ('size', 'Size'),
-    ('time', 'time')
+    ('time', 'Time'),
 )
+
 
 class BackupObject(models.Model):
     """Backup Object
-    
+
     App to be backed up.  All apps from INSTALLED APPS are added upon `syncdb`.
-    
+
     """
 
     app_label = models.CharField(blank=True, max_length=100, unique=True)
-    include = models.BooleanField(default=True, help_text='Include this app when performing backup?')
+    include = models.BooleanField(default=True,
+        help_text='Include this app when performing backup?')
     use_natural_keys = models.BooleanField(default=True)
     compress = models.BooleanField(default=True)
-    prune_by = models.CharField(max_length=4, choices=PRUNE_CHOICES, default='count',
+    prune_by = models.CharField(max_length=4, choices=PRUNE_CHOICES,
+        default='count',
         help_text='What factor leads to archive file deletion?')
-    prune_value = models.CharField(blank=True, null=True, max_length='255', default='10')
+    prune_value = models.CharField(blank=True, null=True, max_length='255',
+        default='10')
     send_to_admins = models.BooleanField(default=True)
     created = models.DateTimeField(blank=True, auto_now_add=True)
     modified = models.DateTimeField(blank=True, auto_now=True)
@@ -40,18 +45,29 @@ class BackupObject(models.Model):
     def prune(self):
         if self.prune_by == 'count':
             count = int(self.prune_value)
-            if BackupArchive.objects.filter(backup_object=self, keep=False).count() > count:
-                last_backup = BackupArchive.objects.filter(backup_object=self, keep=False).only('created')[count-1]
+            if BackupArchive.objects.filter(backup_object=self,
+                keep=False).count() > count:
+                last_backup = BackupArchive.objects.filter(backup_object=self,
+                    keep=False).only('created')[count-1]
                 BackupArchive.objects.filter(
                     backup_object=self,
                     keep=False,
-                    created__lt=last_backup.created
-                ).delete()
-
+                    created__lt=last_backup.created).delete()
+        elif self.prune_by == 'size':
+            max_size = int(self.prune_value)
+            qs = BackupArchive.objects.filter(backup_object=self,
+                keep=False).aggregate(Sum('size'))
+            size = qs['size__sum']
+            while size > max_size:
+                archive = BackupArchive.objects.filter(
+                    backup_object=self).order_by('created')[0]
+                size = size - archive.size
+                archive.delete()
 
     def backup(self, notes=None):
         dt = datetime.datetime.now()
-        name = u'%s_%s%s.%s' % (self.app_label, dt.strftime('%Y%j-'), int(time.time()), FORMAT)
+        name = u'%s_%s%s.%s' % (self.app_label, dt.strftime('%Y%j-'),
+            int(time.time()), FORMAT)
 
         if self.compress:
             name = name + u'.gz'
@@ -63,25 +79,20 @@ class BackupObject(models.Model):
             else:
                 b_file = open(path, 'w')
             dump = dumpdata.Command()
-            b_file.write(
-                dump.handle(
+            b_file.write(dump.handle(
                     self.app_label,
                     use_natural_keys=self.use_natural_keys,
                     indent=INDENT,
-                    format=FORMAT
-                )
-            )
+                    format=FORMAT))
             b_file.close()
             BackupArchive.objects.create(
                 backup_object=self,
                 name=name,
                 path=path,
                 size=os.path.getsize(path),
-                notes=notes
-            )
+                notes=notes)
         except IOError:
             pass
-
 
     def __unicode__(self):
         return self.app_label
@@ -98,7 +109,6 @@ class BackupArchive(models.Model):
     keep = models.BooleanField(default=False)
     edited = models.DateTimeField(blank=True, auto_now=True)
     created = models.DateTimeField(blank=True, auto_now_add=True)
-    
 
     def __unicode__(self):
         return u"%s on %s" % (self.backup_object, self.created.isoformat())
